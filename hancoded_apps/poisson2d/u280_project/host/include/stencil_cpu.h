d61f06d9ef95a5b707a5058cd74c00673fe0e600
Beniel Thileepan <thileepanbeniel@gmail.com>
2025-08-01 17:30:10 +0300

    [SW_EMU_TESTED] added poisson2d handcoded u280 project with make and run scripts

---
+++ hancoded_apps/poisson2d/u280_project/host/include/stencil_cpu.h
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+struct Grid_Parameter{
+
+	unsigned int logical_size_x;
+	unsigned int logical_size_y;
+
+	unsigned int act_size_x;
+	unsigned int act_size_y;
+
+	unsigned int grid_size_x;
+	unsigned int grid_size_y;
+
+    unsigned int num_iter;
+	unsigned int batch;
+
+};
+
+int stencil_computation(float* current, float* next, Grid_Parameter data_g);
+double square_error(float* current, float* next, Grid_Parameter data_g);
+int copy_grid(float* grid_s, float* grid_d, Grid_Parameter data_g);
+int initialise_grid(float* grid, Grid_Parameter data_g);
+
+
