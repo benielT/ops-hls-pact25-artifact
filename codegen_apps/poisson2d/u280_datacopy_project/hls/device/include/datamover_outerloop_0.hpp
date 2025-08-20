d0be9acb40d6da13a26dd6ebcb162225ad5cdf54
Beniel Thileepan <thileepanbeniel@gmail.com>
2025-08-20 14:28:01 +0300

    [HW_TESTED] poisson2d codegen u280 datacopy batched profile, build artifacts added

---
+++ codegen_apps/poisson2d/u280_datacopy_project/hls/device/include/datamover_outerloop_0.hpp
@@ -0,0 +1,27 @@
+// Auto-generated at 2025-08-20 00:23:44.014257 by ops-translator
+
+#pragma once
+#include <ops_hls_kernel_support.h>
+#include "common_config.hpp"
+
+
+extern "C" void datamover_outerloop_0(
+        const bool is_loopback,
+        const unsigned short range_start_0,
+        const unsigned short range_end_0,
+        const unsigned short range_start_1,
+        const unsigned short range_end_1,
+        const unsigned short gridSize_0,
+        const unsigned short gridSize_1,
+        const unsigned int outer_itr,
+        const unsigned short batch_size,
+    //u
+        ap_uint<mem_data_width>* arg0,
+    //u2
+        ap_uint<mem_data_width>* arg1,
+    //u
+        hls::stream <ap_axiu<axis_data_width,0,0,0>>& arg0_axis_out,
+    //u2
+        hls::stream <ap_axiu<axis_data_width,0,0,0>>& arg1_axis_in
+    )
+;
---
+++ codegen_apps/poisson2d/u280_datacopy_project/hls/host/kernel_wrappers/hls_kernels.hpp
@@ -0,0 +1,29 @@
+// Auto-generated at 2025-08-20 00:23:43.659264 by ops-translator
+
+#pragma once
+
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <ops_hls_rt_support.h>
+
+extern float dx;
+extern float dy;
+extern float dy_2;
+extern float dx_2;
+extern float dx_2_plus_dy_2_mult_2;
+extern float dx_2_dy_2;
+
+typedef float stencil_type;
+constexpr unsigned short vector_factor = 8;
+constexpr unsigned short mem_vector_factor = 16;
+constexpr unsigned int iter_par_factor = 27;
+
+// user kernel files
+#include "poisson_kernel_populate_kernel.hpp"
+#include "poisson_kernel_update_kernel.hpp"
+#include "poisson_kernel_initialguess_kernel.hpp"
+#include "outerloop_0.hpp"
+
